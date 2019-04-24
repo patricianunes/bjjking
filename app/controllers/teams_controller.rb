421224fbe3602356b33f9e6a64c49b7b7bcf27c0
Patricia Campos <patricia@krint.io>
2019-04-24 12:38:01 +1000

    fix edit form team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,7 @@
 class TeamsController < ApplicationController
-  before_action :set_team, only: [:show, :edit, :update, :destroy]
+  before_action :set_team, except: [:index, :new, :create]
   before_action :check_for_login, :only => [:new, :create]
+  before_action :check_for_admin, :only => [:edit]
 
   def index
     @teams = Team.all
@@ -9,17 +10,7 @@ class TeamsController < ApplicationController
   def show
   end
 
-  def new
-    @team = Team.new
-  end
-
   def edit
-  end
-
-  def create
-    @team = Team.new(team_params)
-    @team.save
-    redirect_to teams_path
   end
 
   def update
@@ -27,17 +18,30 @@ class TeamsController < ApplicationController
     redirect_to team_path(@team.id)
   end
 
+  def new
+    @team = Team.new
+  end
+
   def destroy
     @team.destroy
     redirect_to teams_path
   end
 
+  def create
+    @team = Team.new team_params
+    @team.save
+    redirect_to teams_path
+  end
+
+
   private
-    def set_team
-      @team = Team.find(params[:id])
-    end
 
     def team_params
-      params.require(:team).permit(:name, :logo, :address)
+      params.require(:team).permit(:id, :name, :logo, :address)
     end
+
+    def set_team
+      @team = Team.find params[:id]
+    end
+
 end
